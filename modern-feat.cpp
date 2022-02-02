d85c3fa418108bbf7dbf1f4544dc8d7867e48568
billamiable <yujie.wang@intel.com>
2022-02-02 17:19:31 +0800

    checked type alias

--- modern-feat.cpp
+++ modern-feat.cpp
@@ -810,16 +810,12 @@ void test301_move_with_nonmove()
 namespace jj48 {
 //-----------
 // http://en.cppreference.com/w/cpp/language/type_alias
-// 本质上和typedef是一样的
-// 这个看起来很怪，但其实是一个函数名的alias
-// 在C++1.0时写作：
-// typedef void (*func)(int, int);
-// 但是上面的写法看起来不太明显，func是一个类型
-// 下面这样写就比较清楚了
+// 功能上与typedef相同，下面是函数名的alias
+// 在C++1.0时写作：typedef void (*func)(int, int);
+// 但是以上写法不容易看清，下面这样写就比较清楚了
 using func = void (*)(int, int);
 
-// 使用时只要赋给一个函数名称即可，本质是一个函数指针
-// 下面创建了对象
+// 下面创建了对象，本质是一个函数指针
 void example(int, int) {}
 func fn = example;
 
@@ -834,28 +830,24 @@ using mystring = std::basic_string<CharT, std::char_traits<CharT>>;
 mystring<char> str;
 // 其實在 <string>, <string_fwd.h> 都有以下 typedef
 // typedef basic_string<char>   string;
-// typedef相当于是c++1.0的实现方法，using更高级了
 
 //-----------
 // 模板类里也可以用这个
+// TODO: 有什么实际场景的用处？
 // type alias can introduce a member typedef name
 template <typename T>
-// 把struct当做class来看
 struct Container {
-    // 相当于这个类里有一个成员叫做value_type
+    // 相当于类里有一个成员叫做value_type
     using value_type = T;  // same as typedef T value_type;
 };
 // which can be used in generic programming
 template <typename Container>
 void fn2(const Container& c)
 {
-    // 强行告诉C++下面这个是类型，不是变量
-    // 这样不用等到实例化再来确定是啥类型
-    // 可能是创建了一个n的对象，把typename去掉来看，单纯认为是表明后者是类型
+    // 使用typename是为了告诉编译器后面的表示类型，而不是变量
     // typename Container::value_type n; // 原版
 
-    // 下面是我自己写的，会用到隐式类型转换
-    // TODO: 这里对于实际的用处不理解？猜测可以用创建的对象去执行后续操作
+    // 下面是新加的测试，会用到隐式类型转换
     typename Container::value_type n = 1;  // n = 1.5
     cout << "value is " << n << endl;
 }
